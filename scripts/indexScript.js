47b382c6c33c7d822e5bf48603fb007362ed6e97
Udit Narayan Sia <udit.sia@gmail.com>
2018-09-08 17:25:54 +0530

    minor style changes

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -2,19 +2,22 @@ window.onload =init();
 function init(){
 
     document.querySelector("#s1").addEventListener("click", function(evt){
-
+       
        let authSub1 = document.querySelector("#authSub1");
-       authSub1.style="left:-950px;opacity:0;";
+       authSub1.style="left:-700px;opacity:0;";
        let authSub2 = document.querySelector("#authSub2");
        authSub2.style="left:30%;opacity:1;";
-       setInterval(function(){ document.querySelector("#pass").focus();},1000);
+       setTimeout(function(){ document.querySelector("#pass").focus();},1000);
        
        //HR width
-       document.querySelector(".hr1").style="width:100%;";
+       let hr = document.querySelector(".hr1");
+       hr.style="width:100%;";
+       setTimeout(function(){hr.style="width:60%;"},1000);
     });
     
     document.querySelector("#back").addEventListener("click",function(){
         authSub1.style="left:30%;opacity:1;";
-        authSub2.style="left:950px;opacity:0;";
-    })
+        authSub2.style="left:948px;opacity:0;";
+        document.querySelector("#email").focus();
+    });
 }
