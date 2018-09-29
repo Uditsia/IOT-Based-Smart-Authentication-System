7f4c5e777d92b2039a7847c6c5881eef0b659de5
Udit Narayan Sia <udit.sia@gmail.com>
2018-09-30 00:39:54 +0530

    landscape image fixed

--- scripts/streamScript.js
+++ scripts/streamScript.js
@@ -1,12 +1,10 @@
 window.onload = init();
 function init() {
   let liveStream = document.querySelector("#lStream");
-  let time = document.querySelector("#timeView label");
-  let d = new Date();
-  setInterval(function() {
-    time.innerHTML =
-      "Date:" + Date().slice(0, 16) + "Time:" + Date().slice(15, 25);
-  }, 300);
-
-  
+  //   let time = document.querySelector("#timeView label");
+  //   let d = new Date();
+  //   setInterval(function() {
+  //     time.innerHTML =
+  //       "Date:" + Date().slice(0, 16) + "Time:" + Date().slice(15, 25);
+  //   }, 300);
 }
