f179a81089db5174059554ca27724abd42447231
Udit Narayan Sia <udit.sia@gmail.com>
2018-10-03 01:28:20 +0530

    updated design

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,40 +1,45 @@
-window.onload =init();
-function init(){
+window.onload = init();
+function init() {
+  document.querySelector("#s1").addEventListener("click", function(evt) {
+    let email = document.querySelector("#email");
+    let emailCheck = email.checkValidity();
+    if (emailCheck) {
+      sub1Transition();
+    } else {
+      let eContainer = document.querySelector(".containerEmail");
+      let errorB1 = document.querySelector(".containerEmail button");
+      errorB1.textContent = "Invalid Email!";
+      eContainer.style = "background-color:red;";
+      errorB1.style = "background-color:red;";
+      email.focus();
+      email.addEventListener("input", function() {
+        errorB1.textContent = "Next";
+        errorB1.style = "background-color:#6200ea;";
+        eContainer.style = "background-color:#6200ea;";
+      });
+    }
 
-    document.querySelector("#s1").addEventListener("click", function(evt){
+    function sub1Transition() {
+      let authSub1 = document.querySelector("#authSub1");
+      authSub1.style = "left:-700px;opacity:0;";
+      let authSub2 = document.querySelector("#authSub2");
+      authSub2.style = "left:30%;opacity:1;";
+      setTimeout(function() {
+        document.querySelector("#pass").focus();
+      }, 1000);
 
-        let email = document.querySelector("#email");
-        let emailCheck = email.checkValidity()
-        if (emailCheck){
-            sub1Transition();
-        }else{
-            
-        }
+      //HR width
+      let hr = document.querySelector(".hr1");
+      hr.style = "width:100%;";
+      setTimeout(function() {
+        hr.style = "width:60%;";
+      }, 1000);
+    }
+  });
 
-
-
-
-       function sub1Transition(){
-       let authSub1 = document.querySelector("#authSub1");
-       authSub1.style="left:-700px;opacity:0;";
-       let authSub2 = document.querySelector("#authSub2");
-       authSub2.style="left:30%;opacity:1;";
-       setTimeout(function(){ document.querySelector("#pass").focus();},1000);
-       
-       //HR width
-       let hr = document.querySelector(".hr1");
-       hr.style="width:100%;";
-       setTimeout(function(){hr.style="width:60%;"},1000);
-       }
-
-    });
-    
-    document.querySelector("#back").addEventListener("click",function(){
-        authSub1.style="left:30%;opacity:1;";
-        authSub2.style="left:948px;opacity:0;";
-        document.querySelector("#email").focus();
-    });
-
-
-
+  document.querySelector("#back").addEventListener("click", function() {
+    authSub1.style = "left:30%;opacity:1;";
+    authSub2.style = "left:948px;opacity:0;";
+    document.querySelector("#email").focus();
+  });
 }
