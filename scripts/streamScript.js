800fdfe5cf2009af328bf583e1ea19601e80226e
Udit Narayan Sia <udit.sia@gmail.com>
2018-10-08 10:24:01 +0530

    liveStream loading added

--- scripts/streamScript.js
+++ scripts/streamScript.js
@@ -1,18 +1,11 @@
 window.onload = init();
 function init() {
   let liveStream = document.querySelector("#lStream");
-  //   let time = document.querySelector("#timeView label");
-  //   let d = new Date();
-  //   setInterval(function() {
-  //     time.innerHTML =
-  //       "Date:" + Date().slice(0, 16) + "Time:" + Date().slice(15, 25);
-  //   }, 300);
+  var img = document.querySelector("img");
+  let loadingContainer = document.querySelector(".container");
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log("Loged in as:" + user);
-    } else {
-      window.location = "../index.html";
-    }
-  });
+  img.onload = () => {
+    loadingContainer.style = "display:none;";
+    console.log("streaming");
+  };
 }
