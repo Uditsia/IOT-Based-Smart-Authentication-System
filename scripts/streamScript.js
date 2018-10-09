976b5cd4946233d954353d18c385aec676e601c2
Udit Narayan Sia <udit.sia@gmail.com>
2018-10-09 15:29:35 +0530

    update

--- scripts/streamScript.js
+++ scripts/streamScript.js
@@ -1,23 +1,5 @@
 window.onload = init();
 function init() {
-  let liveStream = document.querySelector("#lStream");
-  var img = document.querySelector("img");
-  let loadingContainer = document.querySelector(".container");
-  let imgUrl = document.querySelector("#cam_ip").innerHTML;
-  liveStream.src = "" + imgUrl + "/video_feed";
-
-  if (imgUrl != "") {
-    loadingContainer.style = "display:none;";
-
-    document.querySelector(".liveTxt").textContent = "LIVE";
-    console.log("streaming");
-  } else {
-    setTimeout(() => {
-      if (document.querySelector(".liveTxt").textContent !== "LIVE") {
-        location.reload();
-      }
-    }, 20000);
-  }
   var camResponse;
   var camOrder = "status";
   getCamStatus();
@@ -39,11 +21,31 @@ function init() {
     });
   }
 
+  let liveStream = document.querySelector("#lStream");
+  var img = document.querySelector("img");
+  let loadingContainer = document.querySelector(".container");
+  let imgUrl = document.querySelector("#cam_ip").innerHTML;
+  liveStream.src = "" + imgUrl + "/video_feed";
+
+  if (imgUrl != "") {
+    loadingContainer.style = "display:none;";
+
+    document.querySelector(".liveTxt").textContent = "LIVE";
+    console.log("streaming");
+  } else {
+    setTimeout(() => {
+      if (document.querySelector(".liveTxt").textContent !== "LIVE") {
+        location.reload();
+      }
+    }, 20000);
+  }
+
   let camStatsBtn = document.querySelector(".camStatsBtn");
   camStatsBtn.addEventListener("click", checkCamStats);
   function checkCamStats() {
     getCamStatus();
     if (camResponse == "True") {
+      alert("cought");
     }
   }
   let camBtn = document.querySelector(".camStatsBtn");
