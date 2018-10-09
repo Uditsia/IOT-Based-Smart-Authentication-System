74ae77bc54b86c633cb41125e7e9f9c979a9d209
Udit Narayan Sia <udit.sia@gmail.com>
2018-10-09 11:40:15 +0530

    cameraBtn added

--- scripts/streamScript.js
+++ scripts/streamScript.js
@@ -6,17 +6,6 @@ function init() {
   let imgUrl = document.querySelector("#cam_ip").innerHTML;
   liveStream.src = "" + imgUrl + "/video_feed";
 
-  /* liveStream.onSuccess = () => {
-    loadingContainer.style = "display:none;";
-    document.querySelector(".liveTxt").textContent = "LIVE";
-    console.log("streaming");
-  };
-  setTimeout(() => {
-    if (document.querySelector(".liveTxt").textContent !== "LIVE") {
-      location.reload();
-    }
-  }, 20000);*/
-
   if (imgUrl != "") {
     loadingContainer.style = "display:none;";
 
@@ -29,4 +18,41 @@ function init() {
       }
     }, 20000);
   }
+  var camResponse;
+  var camOrder = "status";
+  getCamStatus();
+
+  function getCamStatus() {
+    var url = imgUrl;
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: url + "/capture_camera?arg=" + camOrder,
+      method: "GET",
+      headers: {
+        "Cache-Control": "no-cache"
+      }
+    };
+    $.ajax(settings).done(function(response) {
+      console.log(response);
+      camResponse = response;
+    });
+  }
+
+  let camStatsBtn = document.querySelector(".camStatsBtn");
+  camStatsBtn.addEventListener("click", checkCamStats);
+  function checkCamStats() {
+    getCamStatus();
+    if (camResponse == "True") {
+    }
+  }
+  let camBtn = document.querySelector(".camStatsBtn");
+  camBtn.addEventListener("click", () => {
+    let camB1 = document.querySelector(".camB1");
+    camB1.style = "margin-left:60%;color:red;";
+    camB1.textContent = "off";
+    let camTxt = document.querySelector(".camArmed");
+    camTxt.textContent = "Disarmed";
+    camTxt.style = "color:red;";
+  });
 }
